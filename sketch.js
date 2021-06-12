653d7909121a182b63e5c0f8c37e1fbd4f8df9bb
Dhriti Dhall <74966124+DhRiTiD@users.noreply.github.com>
2021-06-13 00:13:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function preload() {
   girlI = loadAnimation("Run (1).png", "Run (2).png", "Run (3).png", "Run (4).png", "Run (5).png", "Run (6).png", "Run (7).png", "Run (8).png", "Run (9).png", "Run (10).png", "Run (11).png", "Run (12).png", "Run (14).png", "Run (15).png", "Run (16).png", "Run (17).png", "Run (18).png", "Run (19).png", "Run (20).png");
   restartI = loadImage("Gameo.png");
   
-  sound = loadSound("Alan Walker - Spectre [NCS Release].mp3");
+  //sound = loadSound("Alan Walker - Spectre [NCS Release].mp3");
 }
 
 function setup() {
